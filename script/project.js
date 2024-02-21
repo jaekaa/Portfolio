5c615984e36d4b7b01df815f648edda2888d094f
nikkzzzyu <nikzzzlumaguemerjoy@gmail.com>
2024-02-21 20:27:57 +0800

    Added Navigation Home

--- script/project.js
+++ script/project.js
@@ -8,4 +8,46 @@
     }
 
     document.addEventListener('click',activate,false);
+
+    var theToggle = document.querySelector('.custom-toggle');
+    var theMenu = document.querySelector('.custom-menu');
+
+    // hasClass
+    function hasClass(elem, className) {
+        return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+    }
+    // addClass
+    function addClass(elem, className) {
+        if (!hasClass(elem, className)) {
+            elem.className += ' ' + className;
+        }
+    }
+    // removeClass
+    function removeClass(elem, className) {
+        var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, ' ') + ' ';
+        if (hasClass(elem, className)) {
+            while (newClass.indexOf(' ' + className + ' ') >= 0) {
+                newClass = newClass.replace(' ' + className + ' ', ' ');
+            }
+            elem.className = newClass.replace(/^\s+|\s+$/g, '');
+        }
+    }
+    // toggleClass
+    function toggleClass(elem, className) {
+        var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, " ") + ' ';
+        if (hasClass(elem, className)) {
+            while (newClass.indexOf(" " + className + " ") >= 0) {
+                newClass = newClass.replace(" " + className + " ", " ");
+            }
+            elem.className = newClass.replace(/^\s+|\s+$/g, '');
+        } else {
+            elem.className += ' ' + className;
+        }
+    }
+
+    theToggle.onclick = function() {
+        toggleClass(this, 'on');
+        toggleClass(theMenu, 'on'); // Toggle the menu along with the toggle button
+        return false;
+    }
 });
